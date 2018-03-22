8450f2b9be5abfb9d4c895c8ecbb6974f3d5de43
Aditya Chopra <adichopra@gmail.com>
2018-03-21 19:36:04 -0700

    fixed

--- setup.py
+++ setup.py
@@ -2,25 +2,20 @@ import random
 import traceback
 import inspect
 import math
+import client
 from servers import StorageServer, PublicKeyServer
 from crypto import Crypto
 from base_client import IntegrityError
 from run_part1_tests import run_part1_tests
 
-argspec = inspect.getargspec(t)
-if argspec.defaults is None:
-    types = {}
-else:
-    types = dict(zip(argspec.args[-len(argspec.defaults):],
-                     argspec.defaults))
-
-server = types['server']() if 'server' in types else StorageServer()
-pks = types['pks']() if 'pks' in types else PublicKeyServer()
-crypto = types['crypto']() if 'crypto' in types else Crypto()
-myclient = __import__(self.theclass, fromlist=[''])
+server = StorageServer()
+pks = PublicKeyServer()
+crypto = Crypto()
 
 def C(name):
-    return myclient.Client(server, pks, crypto, name)
+    return client.Client(server, pks, crypto, name)
+
+# example test1:
 
 # alice = C("alice")
 # bob = C("bob")
